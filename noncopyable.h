8371efdb90b5fd666c02b6e7c2f44461f50df8d8
jade32 <1617459108@qq.com>
2024-08-26 23:16:58 +0800

    8.26晚上代码更新, 更新了不可赋值与拷贝构造的noncopyable 更新了日志编写类

---
+++ noncopyable.h
@@ -0,0 +1,16 @@
+#pragma once
+/*
+noncopyable被继承后，无法进行拷贝构造和赋值操作
+但是其派生类可以进行正常的构造和机构
+*/
+
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable &) = delete;
+    noncopyable &operator=(const noncopyable &) = delete;
+
+private:
+    noncopyable() = default;
+    ~noncopyable() = default;
+};
